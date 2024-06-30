cbd352b6e4f117df4836baa2c82e47ba18dd31f8
dacods <135403583+dacods@users.noreply.github.com>
2024-06-30 17:14:02 -0500

    Created class DBStorage, def __init__, and def all

---
+++ models/engine/db_storage.py
@@ -0,0 +1,51 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class DBStorage:
+    """Defines a class"""
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initalize DBStorage"""
+        user = os.getenv('HBNB_MY_SQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        database = os.getenv('HBNB_MYSQL_DB')
+        env = os.getenv('HBNB_ENV')
+
+        self.__engine = create_engine(
+            f'mysql+mysqldb://{user}:{password}@{host}/{database}',
+            pool_pre_ping=True
+        )
+
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
+        
+    def all(self, cls=None):
+        """Query all objects on class name"""
+        obj_dict = {}
+        if cls:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = f"{obj.__class__.__name__}.{obj.id}"
+                obj_dict[key] = obj
+        else:
+            for cls in [User, State, City, Amenity, Place, Review]:
+                objs = self.__session.query(cls).all()
+                for obj in objs:
+                    key = f"{obj.__class__.__name__}.{obj.id}"
+                    obj_dict[key] = obj
+        return obj_dict
+    
+    

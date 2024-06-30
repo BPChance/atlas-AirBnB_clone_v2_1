e8404fe0c455f00405474b9902fbb47ba5258d28
dacods <135403583+dacods@users.noreply.github.com>
2024-06-30 16:50:46 -0500

    Added sqlalchemy to State in state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if storage_t == 'db':
+        cities = relationship('City', backref='state', casade='all, delete, delete-orphane')
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list

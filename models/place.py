304207580b06ada1b985e04c2c19fa0d162cdfcf
BPChance <bradenpchance6@gmail.com>
2024-07-01 14:01:25 -0500

    updated amenity and place.py. many to many relationships, a table, and typos

--- models/place.py
+++ models/place.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy import Column, String, Integer, Float, Foreignkey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from models.review import Review
+from models.amenity import Amenity
 import models
+# place_amenity table for many-to-many relationship
+place_amenity = Table('place_amenity', Base.metadata,
+                              Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                              Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), Foreignkey('cities.id'), nullable=False)
-    user_id = Column(String(60), Foreignkey('users'), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)
@@ -24,16 +29,24 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
+    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
 
     @property
-    def reviews(self):
-        """
-        returns the list of Review instances with
-        place_id equals to the current Place.id
-        """
+    def amenities(self):
+        """ returns list of amenity instances"""
+        from models import storage
+        amenity_list = []
+        for amenity_id in self.amenity_ids:
+            amenity = storage.get(Amenity, amenity_id)
+            if amenity:
+                amenity_list.append(amenity)
+        return amenity_list
+    
+    @amenities.setter
+    def amenities(self, amenity_obj):
+        """ handles append method for adding an Amenity.id """
+        if isinstance(amenity_obj, Amenity):
+            if amenity_obj.id not in self.amenity_ids:
+                self.amenity_ids.append(amenity_obj.id)
+    
 
-        review_list = []
-        for review in models.storage.all(Review).values():
-            if review.place_id == self.id:
-                review_list.append(review)
-        return review_list

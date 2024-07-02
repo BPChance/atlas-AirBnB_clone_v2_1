390b5c72325cd000a695f3ec6651ce4384adf3a0
BPChance <bradenpchance6@gmail.com>
2024-07-02 16:21:02 -0500

    moved table to amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,10 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from models.place import place_amenity
+# place_amenity table for many-to-many relationship
+place_amenity = Table('place_amenity', Base.metadata,
+                              Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                              Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 
 class Amenity(BaseModel, Base):

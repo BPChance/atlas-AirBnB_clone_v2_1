393fda875bdc41fb3fa6254079cf10edbb76f2d3
BPChance <bradenpchance6@gmail.com>
2024-07-02 18:29:39 -0500

    bad indents

--- models/place.py
+++ models/place.py
@@ -8,12 +8,13 @@ import os
 
 # place_amenity table for many-to-many relationship
 place_amenity = Table('place_amenity', Base.metadata,
-                        Column('place_id', String(60),
-                            ForeignKey('places.id'),
-                            primary_key=True, nullable=False),
-                        Column('amenity_id', String(60),
-                            ForeignKey('amenities.id'),
-                            primary_key=True, nullable=False))
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False),
+                      )
 
 
 class Place(BaseModel, Base):

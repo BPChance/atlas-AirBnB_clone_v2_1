47f0754e10914cf04a68f891861009bb424081e0
BPChance <bradenpchance6@gmail.com>
2024-07-02 17:53:54 -0500

    fixing city.py pycodestyle

--- models/city.py
+++ models/city.py
@@ -5,11 +5,11 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
 
-    places = relationship("Place", backref="city", cascade="all, delete-orphan")
+    places = relationship("Place", backref="city",
+                          cascade="all, delete-orphan")

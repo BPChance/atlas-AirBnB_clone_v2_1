fc395361bfd0ed4ba3b3349d6fbf275dbbd4a57c
dacods <135403583+dacods@users.noreply.github.com>
2024-07-02 14:51:19 -0500

    Fixed missing Base in review

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
 

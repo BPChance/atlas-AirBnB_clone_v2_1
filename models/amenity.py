a7e021ee05382c326dae5574af3cdab7e284bd24
dacods <135403583+dacods@users.noreply.github.com>
2024-07-02 16:46:29 -0500

    Made changes in amentit

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models import place_amenity
 
 
 
@@ -11,4 +12,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
 
-    place_amenities = relationship("Place", secondary='place_amenity', back_populates="amenities")
+    place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")

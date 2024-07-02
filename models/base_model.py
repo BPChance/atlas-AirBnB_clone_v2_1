fc912139e290e6a3439c99c439299d4df9771b94
BPChance <bradenpchance6@gmail.com>
2024-07-02 18:14:20 -0500

    more pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,10 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = Column(String(60), primary_key=True, nullable=False, unique=True, default=lambda: str(uuid.uuid4()))
+    id = Column(String(60), primary_key=True,
+                nullable=False, unique=True,
+                default=lambda: str(uuid.uuid4())
+                )
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 

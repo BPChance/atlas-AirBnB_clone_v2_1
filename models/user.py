fd61eb504c840702a203467d6583b633c1fb05f3
BPChance <bradenpchance6@gmail.com>
2024-07-02 20:14:19 -0500

    trying to fix models

--- models/user.py
+++ models/user.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import models.base_model
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models.base_model import storage_type
+import models
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    if models.base_model == 'db':
+        __tablename__ = 'users'
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-    places = relationship("Place", backref="user",
-                          cascade="all, delete-orphan")
-    reviews = relationship("Review", backref="user",
-                           cascade="all, delete-orphan")
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete-orphan")
+
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''

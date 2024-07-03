b14d74c7b14363c39baa83a88fba07a48944eb0a
BPChance <bradenpchance6@gmail.com>
2024-07-02 21:17:31 -0500

    pycode i guess

--- models/base_model.py
+++ models/base_model.py
@@ -9,15 +9,16 @@ Base = declarative_base()
 
 
 class BaseModel:
-     """A base class for all hbnb models"""
-     id = Column(String(60), primary_key=True,
+    """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True,
                 nullable=False, unique=True,
                 default=lambda: str(uuid.uuid4())
                 )
-     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
-     def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
@@ -33,19 +34,19 @@ class BaseModel:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-     def __str__(self):
+    def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
-     def save(self):
+    def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
-     def to_dict(self):
+    def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
@@ -57,7 +58,7 @@ class BaseModel:
             del dictionary['_sa_instance_state']
         return dictionary
 
-     def delete(self):
+    def delete(self):
         """Deletes the current instance from the storage"""
         from models import storage
         storage.delete(self)
